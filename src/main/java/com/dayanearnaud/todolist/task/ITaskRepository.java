817ce3f4f8ac40b12d24c12588b8778cb24c207e
DayaneArnaud <dayanearnaud@gmail.com>
2024-06-18 20:31:03 -0300

    User e Tasks funcionalidades

---
+++ src/main/java/com/dayanearnaud/todolist/task/ITaskRepository.java
@@ -0,0 +1,7 @@
+package com.dayanearnaud.todolist.task;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+}
