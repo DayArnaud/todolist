2de540ef1c3899caff411ce50501a76d4c03bfd9
DayaneArnaud <dayanearnaud@gmail.com>
2024-06-19 11:13:33 -0300

    Gernado a autenticação do usuário

--- src/main/java/com/dayanearnaud/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/dayanearnaud/todolist/filter/FilterTaskAuth.java
@@ -3,8 +3,14 @@ package com.dayanearnaud.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.dayanearnaud.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,22 +18,39 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-                var authorization = request.getHeader("Authorization");
-                var authEncoded = authorization.substring("Basic".length()).trim();
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-                var authString = new String(authDecode);
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+                var servletPath = request.getServletPath();
 
-                System.out.println(username);
-                System.out.println(password);
+                if(servletPath.equals("/tasks/")) {
+                    var authorization = request.getHeader("Authorization");
+                    var authEncoded = authorization.substring("Basic".length()).trim();
+                    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+                    var authString = new String(authDecode);
 
-                filterChain.doFilter(request, response);
-    }
+                    String[] credentials = authString.split(":");
+                    String username = credentials[0];
+                    String password = credentials[1];
+
+                    var user = this.userRepository.findByUsername(username);
+                    if(user == null) {
+                        response.sendError(401);
+                    } else {
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        
+                        if(passwordVerify.verified) {
+                            filterChain.doFilter(request, response);
+                        } else {
+                            response.sendError(401);
+                        }
+                    }
+                } else {
+                    filterChain.doFilter(request, response);
+                }  
+            }
 }
